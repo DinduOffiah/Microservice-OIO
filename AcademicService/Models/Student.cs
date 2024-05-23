71e88bfb3d92185f5facd71744e6a573ad306ac2
Chidindu <chidinduoffiah@gmail.com>
2024-05-23 17:16:56 +0100

    Academic Model

---
+++ AcademicService/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace AdministrationService.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Department { get; set; }
+
+    }
+}
