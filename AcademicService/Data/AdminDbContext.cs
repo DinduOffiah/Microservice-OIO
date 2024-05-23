d963f6f7761a363a6e54c79e131e5d48d4325a34
Chidindu <chidinduoffiah@gmail.com>
2024-05-23 17:16:01 +0100

    Acedemic DBContext

---
+++ AcademicService/Data/AdminDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AdministrationService.Models;
+using System.Collections.Generic;
+
+namespace AppDbContext.Data
+{
+    public class AcademicDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public AcademicDbContext(DbContextOptions<AcademicDbContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        //For correctly mapping "Decimal" data type to the corresponding column in the database.
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Bid>().Property(b => b.Amount).HasPrecision(10, 2);
+        //    base.OnModelCreating(modelBuilder);
+
+        //    // This tells Entity Framework to use the User table instead of AspNetUsers for the User entity.
+        //    modelBuilder.Entity<User>().ToTable("User");
+        //}
+
+    }
+}
