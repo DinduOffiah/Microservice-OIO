ce183fa177ebe13545969d3346c9694d32d46edf
Chidindu <chidinduoffiah@gmail.com>
2024-05-23 16:09:33 +0100

    Students Service

---
+++ AdministrationService/Services/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿using AdministrationService.Models;
+
+namespace AdministrationService.Interface
+{
+    public interface IStudentService
+    {
+        Task<Student> RegisterStudentAsync(Student student);
+        Task<IEnumerable<Student>> GetAllStudentsAsync();
+    }
+
+}
---
+++ AdministrationService/Services/StudentService.cs
@@ -0,0 +1,43 @@
+﻿using AppDbContext.Data;
+using Microsoft.EntityFrameworkCore;
+using RegisterService.Interface;
+using RegisterService.Models;
+
+namespace RegisterService.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly RegisterDbContext _context;
+        private readonly INotificationService _notificationService;
+
+
+        public StudentService(RegisterDbContext context, INotificationService notificationService)
+        {
+            _context = context;
+            _notificationService = notificationService;
+        }
+
+        public async Task<Patient> RegisterPatientAsync(Patient patient)
+        {
+            patient.Id = Guid.NewGuid();
+            _context.Patients.Add(patient);
+            await _context.SaveChangesAsync();
+
+            // Notify VitalService about new patient
+            await _notificationService.NotifyNewPatientAsync(patient);
+
+            return patient;
+        }
+
+        public async Task<Patient> GetPatientAsync(Guid id)
+        {
+            return await _context.Patients.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Patient>> GetAllPatientsAsync()
+        {
+            return await _context.Patients.ToListAsync();
+        }
+    }
+
+}
